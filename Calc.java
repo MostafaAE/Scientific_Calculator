6d507d2e02dffc58b72893e7ec6b26010c8b198d
Mostafa Amin <47722373+MostafaAmin0@users.noreply.github.com>
2021-04-05 01:23:19 +0200

    Driver Class

---
+++ Calc.java
@@ -0,0 +1,13 @@
+
+
+public class Calc {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+         CalcFrame calculator = new CalcFrame();
+
+    }
+
+}
